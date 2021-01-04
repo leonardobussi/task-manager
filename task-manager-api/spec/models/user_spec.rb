483cd764328ee23ab0ad074581f144e34a565d3b
leonardobussi <leonardobezerrabussy@gmail.com>
2021-01-04 16:14:01 -0300

    implemenented test of factoryGirlRails

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  before { @user = FactoryGirl.build(:user) }
+  # before { @user = FactoryGirl.build(:user) }
 
-  it { expect(@user).to respond_to(:email)  }
+  # it { expect(@user).to respond_to(:email)  }
+
+  # subject { build(:user) } 
+  # it { expect(subject).to respond_to(:email)  }
+  # it { expect(subject).to be_valid  }
+
+
+  it { is_expected.to respond_to(:email)  }
 end
