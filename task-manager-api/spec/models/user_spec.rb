1e7f0a1537b7d3400e6678fcc5d6bde44001c69c
leonardobussi <leonardobezerrabussy@gmail.com>
2021-01-04 15:34:24 -0300

    configure as

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  it { expect(@user).to respond_to(:email)  }
 end
