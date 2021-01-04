c753281afc73845a09c99f3764f74c6c4aff2247
leonardobussi <leonardobezerrabussy@gmail.com>
2021-01-04 17:30:05 -0300

    finish shouda

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  # before { @user = FactoryGirl.build(:user) }
-
-  # it { expect(@user).to respond_to(:email)  }
-
-  # subject { build(:user) } 
-  # it { expect(subject).to respond_to(:email)  }
-  # it { expect(subject).to be_valid  }
-
-
-  it { is_expected.to respond_to(:email)  }
+  let!(:user) {build(:user)}
+  
+  it { is_expected.to validate_presence_of(:email)   }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive   }
+  it { is_expected.to validate_confirmation_of(:password)   }
+  it { is_expected.to allow_value("leonardo@bussi.com").for(:email)   }
 end
