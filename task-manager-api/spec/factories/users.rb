6eded87279c39ea40090663525553af818059b9e
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-05 16:14:37 -0300

    update the method at the factory girl

--- task-manager-api/spec/factories/users.rb
+++ task-manager-api/spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
     factory :user do
-        email "leonardo@bussi.com"
+        email { Faker::Internet.email }
         password "123456789"
         password_confirmation "123456789"
     end
