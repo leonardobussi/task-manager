7a4e1a5818234a30b3068eff8f2652651adb1cc8
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-06 17:06:53 -0300

    falta arrumar os testes

--- task-manager-api/config/routes.rb
+++ task-manager-api/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   namespace 'api' do 
     namespace 'v1' do
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :sessions, only: [:create]
     end
   end
 end
