d8a7338a83bf9af5dee608b1b02b100f99e2de19
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-06 14:55:20 -0300

    adding test in model of user

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -10,9 +10,10 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:auth_token)   }
 
   describe "#info" do 
-    it "retornar o email e o created_at" do 
+    it "retornar o email, created_at e o token" do 
       user.save!
-      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+      allow(Devise).to receive(:friendly_token).and_return('wNDqryugpFxsmNZShBiF')
+      expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{Devise.friendly_token}")
     end
   end
 
