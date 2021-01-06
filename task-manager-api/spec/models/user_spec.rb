228c8bea3cecb2fd04329a6409b8959c4271c97f
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-06 14:34:04 -0300

    o resto

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive   }
   it { is_expected.to validate_confirmation_of(:password)   }
   it { is_expected.to allow_value("leonardo@bussi.com").for(:email)   }
+  it { is_expected.to validate_uniqueness_of(:auth_token)   }
+
+  describe "#info" do 
+    it "retornar o email e o created_at" do 
+      user.save!
+      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    end
+  end
+
 end
