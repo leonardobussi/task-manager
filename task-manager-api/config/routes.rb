58d90b45a407e55a350e64c403184cf3503e96a7
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-07 10:32:49 -0300

    adding verb at the session of user

--- task-manager-api/config/routes.rb
+++ task-manager-api/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace 'api' do 
     namespace 'v1' do
       resources :users, only: [:show, :create, :update, :destroy]
-      resources :sessions, only: [:create]
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
