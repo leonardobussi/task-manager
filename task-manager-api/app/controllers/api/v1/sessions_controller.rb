e297f6fe2095d11e8aa732efb246881e58961246
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-07 08:48:47 -0300

    consertado o erro

--- task-manager-api/app/controllers/api/v1/sessions_controller.rb
+++ task-manager-api/app/controllers/api/v1/sessions_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create 
-    user = User.find_by(email: sessions_params[:email])
+    user = User.find_by(email: session_params[:email])
 
-    if user && user.valid_password?(sessions_params[:password]) 
+    if user && user.valid_password?(session_params[:password]) 
       render json: user, status: 200
     end
   end
 
   private
-  def sessions_params 
+  def session_params 
     params.require(:session).permit(:email, :password)
   end
 end
