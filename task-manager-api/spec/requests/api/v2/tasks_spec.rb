be2cb3a47b460990e25dbda4c7bee1285e5a3555
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-11 16:02:03 -0300

    adding params of search

--- task-manager-api/spec/requests/api/v2/tasks_spec.rb
+++ task-manager-api/spec/requests/api/v2/tasks_spec.rb
@@ -14,18 +14,42 @@ RSpec.describe "Task Api", type: :request do
 
 
   describe "Get /tasks/" do 
-    before do 
-      create_list(:task, 5, user_id: user.id)
-      get "/tasks", params: {}, headers: headers
+    context "when no filter param is sent" do 
+      before do 
+        create_list(:task, 5, user_id: user.id)
+        get "/tasks", params: {}, headers: headers
+      end
+  
+      it 'status code 200' do
+        expect(response).to have_http_status(200)
+      end
+  
+      it 'retornar 5 tasks' do
+        expect(json_body["data"].count).to eq(5)
+      end
     end
 
-    it 'status code 200' do
-      expect(response).to have_http_status(200)
+    context 'when filter params is sent' do 
+      let!(:notbook_task1) { create(:task, title: 'Check if the notebook is broken', user_id: user.id)}
+      let!(:notbook_task2) { create(:task, title: 'Buy a new notebook', user_id: user.id)}
+      # let!(:other_task_1) {create(:task, title: 'Fix the door', user_id: user.id)}
+      # let!(:other_task_2) {create(:task, title: 'Buy a new car', user_id: user.id)}
+
+
+      before do 
+        get '/tasks?q=[title_cont]=note', params: { }, headers: headers
+      end
+
+
+      it 'returns only task matcheing' do
+        returned_task_titles = json_body['data'].map {|t| t['attributes']['title']}
+        expect(returned_task_titles).to eq([notbook_task1.title, notbook_task2.title])
+      end
+
+
     end
 
-    it 'retornar 5 tasks' do
-      expect(json_body["data"].count).to eq(5)
-    end
+   
   end
 
   describe 'GET /tasks/:id' do
