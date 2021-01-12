b49426b84c2efca1ec36c439d737d5b26e642da3
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-12 14:06:50 -0300

    remove

--- task-manager-api/app/models/task.rb
+++
@@ -1,5 +0,0 @@
-class Task < ApplicationRecord
-  belongs_to :user
-
-  validates_presence_of :title, :user_id
-end
--- task-manager-api/spec/support/request_spec_helper.rb
+++
@@ -1,5 +0,0 @@
-module RequestSpecHelper
-  def json_body
-    @json_body ||= JSON.parse(response.body, simbolize_names: true)
-  end
-end
