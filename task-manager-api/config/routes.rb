482ab5af2d7673623c775520ca9620938e9f762a
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-12 13:26:55 -0300

    mudei tudo

--- task-manager-api/config/routes.rb
+++ task-manager-api/config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: :json }, path: "/" do
     namespace :v2, path: "/" do
-      resources :users, only: [:show, :create, :update, :destroy]
-      resources :sessions, only: [:create, :destroy]
-      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      resources :users, path: "/users", only: [:show, :create, :update, :destroy]
+      resources :sessions, path:"/sessions", only: [:create, :destroy]
+      resources :tasks, path:'/tasks', only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
