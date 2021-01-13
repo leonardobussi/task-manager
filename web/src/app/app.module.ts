8880cb4057aeb6f2b4abb43230568ec846995495
leonardo.bussi <leonardo.bussi@leavening.com>
2021-01-13 08:51:59 -0300

    adding component of navbar

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
